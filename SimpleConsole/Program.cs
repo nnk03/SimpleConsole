4627404c7b19944bb12edfcfe2da7eb22914e9ff
NEERAJ KRISHNA N <NEERAJ KRISHNA N@LAPTOP-LFID29O2>
2024-08-27 08:07:16 +0530

    Add project files.

---
+++ SimpleConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using StreamWriterClass;
+
+TextWriter textWriter = new StreamWriter(Console.OpenStandardOutput());
+
+textWriter.WriteLine("Hello, World!");
+
+textWriter.Flush();
+
+textWriter.Close();
+
+TextWriter lowerTextWriter = new LowerStreamWriter(Console.OpenStandardOutput());
+
+lowerTextWriter.WriteLine("Hello, World!");
+lowerTextWriter.Flush();
+
+lowerTextWriter.Close();
---
+++ StreamWriterClass/LowerStreamWriter.cs
@@ -0,0 +1,19 @@
+﻿namespace StreamWriterClass
+{
+    public class LowerStreamWriter : StreamWriter
+    {
+        private string CoreNewLineStr = Environment.NewLine;
+        public LowerStreamWriter(Stream stream) : base(stream)
+        {
+        }
+
+        public override void WriteLine(string? value)
+        {
+            if (value != null)
+            {
+                Write(value.ToLower());
+            }
+            Write(CoreNewLineStr);
+        }
+    }
+}
